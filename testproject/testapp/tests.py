25ca9b1172b8feb05b42e2128aa9b7c41af2b2bf
Yaroslav Shukharev <krkmetal@gmail.com>
2020-04-23 17:10:34 +1000

    fixed test_empty_changelist by replacing test model

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -1,64 +1,64 @@
 from admin_smoke.tests import AdminTests, AdminBaseTestCase
+from django.contrib.auth import get_user_model
 
 from testproject.testapp import admin, models
 
 
 class UserAdminTestCase(AdminTests, AdminBaseTestCase):
-    model_admin = admin.UserAdmin
-    model = models.User
-    object_name = 'user'
+    model_admin = admin.SubjectAdmin
+    model = models.Subject
+    object_name = 'subject'
 
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
-        cls.user = models.User.objects.create_user(
-            username='username',
-            email='email@adminlog.com',
-            password='password'
+        cls.subject = models.Subject.objects.create(
+            title='Subject 1',
+            content='Some test content.'
         )
+        cls.test_user = get_user_model().objects.create(username='test_user')
 
     def transform_to_new(self, data: dict) -> dict:
-        data['username'] += '_new'
-        data['password1'] = 'unique_password'
-        data['password2'] = 'unique_password'
+        data['title'] += '(updated)'
+        data['content'] = 'New test content.'
         return data
 
     def test_created_by_without_admin(self):
         """ Created by is set to None if created outside of django admin."""
         self.assert_object_fields(
-            self.user,
+            self.subject,
             created_by=None)
 
     def test_modified_by_without_admin(self):
         """ Modified by is set to None if modified outside of django admin."""
-        self.user.is_active = False
+        self.subject.title = 'Subject 1(modified)'
 
-        self.user.save()
+        self.subject.save()
 
         self.assert_object_fields(
-            self.user,
+            self.subject,
             modified_by=None)
 
     def test_changeform_create(self):
         """ Created_by/modified_by for new user are set to logger user."""
         super().test_changeform_create()
 
-        user = models.User.objects.last()
+        subject = models.Subject.objects.last()
         self.assert_object_fields(
-            user,
+            subject,
             created_by=self.superuser,
             modified_by=self.superuser)
 
     def test_changeform_save(self):
         """ Created_by remains untouched, modified_by is set to logged user."""
         self.update_object(
-            self.user,
-            created_by=self.user,
-            modified_by=self.user)
+            self.subject,
+            created_by=self.test_user,
+            modified_by=self.test_user)
 
         super().test_changeform_save()
 
         self.assert_object_fields(
-            self.user,
-            created_by=self.user,
+            self.subject,
+            created_by=self.test_user,
             modified_by=self.superuser)
