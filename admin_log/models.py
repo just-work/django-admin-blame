efaa2e11e3885e7a73904f39040d97bc4e49ad75
tumb1er <zimbler@gmail.com>
2020-12-01 13:23:43 +0300

    Remove adding modified to update_fields in save()
    It's default behavior in django-model-utils=4.1.0

--- admin_log/models.py
+++ admin_log/models.py
@@ -17,16 +17,3 @@ class AdminLogMixin(TimeStampedModel):
 
     class Meta:
         abstract = True
-
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        """
-        Force adding modified to update_fields if specified.
-
-        # TODO
-        This will be default behavior for TimeStampedModel in next release after
-        django-model-utils==4.0.0
-        """
-        if update_fields is not None:
-            update_fields = set(update_fields) | {'modified'}
-        super().save(force_insert, force_update, using, update_fields)
